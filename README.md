f11c4fc0014f88bfd5d0e27cf0066931035c5db3
Romi Julianto <romyjulians@gmail.com>
2022-10-03 05:34:23 +0700

    import database to app.module.ts

--- README.md
+++ README.md
@@ -3,13 +3,7 @@ _Documentation by Romi Julianto_
 ## Installation
 
 ```bash
-npm i -g @nestjs/cli
-```
-
-```bash
-nest new backend
-```
-
-```bash
-nest new backend
+- npm i -g @nestjs/cli
+- nest new backend
+= npm i @nestjs/typeorm typeorm mysql2 @nestjs/config
 ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // import module
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT), // import default string format use parseInt
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASS,
+      database: process.env.MYSQL_DB,
+      entities: [
+
+      ],
+      synchronize: true // automate generate table
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
