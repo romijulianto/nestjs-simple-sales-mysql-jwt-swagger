b03ecc89ee77de73bbe27843abd068818deefa8e
Romi Julianto <romyjulians@gmail.com>
2022-10-03 06:11:04 +0700

    create user.entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: Number
+
+    @Column()
+    nama_user: String
+
+    @Column()
+    email: String
+
+    @Column()
+    password: String
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+ }
+
+ 
